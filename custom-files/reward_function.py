62ca8fff0022a444b91529630bb28102928e9955
= <=>
2024-07-08 01:10:51 +0530

    updated the reward function and model name

--- custom-files/reward_function.py
+++ custom-files/reward_function.py
@@ -33,6 +33,7 @@ from scipy import signal
 smoothPath = []
 turnPoints = []
 angleChange = []
+optimalVelocity = []
 
 def calc_distance(prev_point, next_point):
     delta_x = next_point[0] - prev_point[0]
@@ -65,6 +66,84 @@ def smooth_central_line_internal(center_line, max_offset, smoothed_line, pp, p, 
             new_line[i][0] = (0.98 * new_line[i][0]) + (0.02 * center_line[i][0])
             new_line[i][1] = (0.98 * new_line[i][1]) + (0.02 * center_line[i][1])
     return new_line
+
+def circle_radius(coords):
+
+    # Flatten the list and assign to variables (makes code easier to read later)
+    x1, y1, x2, y2, x3, y3 = [i for sub in coords for i in sub]
+
+    a = x1*(y2-y3) - y1*(x2-x3) + x2*y3 - x3*y2
+    b = (x1**2+y1**2)*(y3-y2) + (x2**2+y2**2)*(y1-y3) + (x3**2+y3**2)*(y2-y1)
+    c = (x1**2+y1**2)*(x2-x3) + (x2**2+y2**2)*(x3-x1) + (x3**2+y3**2)*(x1-x2)
+    d = (x1**2+y1**2)*(x3*y2-x2*y3) + (x2**2+y2**2) * \
+        (x1*y3-x3*y1) + (x3**2+y3**2)*(x2*y1-x1*y2)
+
+    # In case a is zero (so radius is infinity)
+    try:
+        r = abs((b**2+c**2-4*a*d) / abs(4*a**2)) ** 0.5
+    except:
+        r = 999
+
+    return r
+
+def circle_indexes(mylist, index_car, add_index_1=0, add_index_2=0):
+
+    list_len = len(mylist)
+
+    # if index >= list_len:
+    #     raise ValueError("Index out of range in circle_indexes()")
+
+    # Use modulo to consider that track is cyclical
+    index_1 = (index_car + add_index_1) % list_len
+    index_2 = (index_car + add_index_2) % list_len
+
+    return [index_car, index_1, index_2]
+
+def optimal_velocity(track, min_speed, max_speed, look_ahead_points):
+    global optimalVelocity
+    if optimalVelocity:
+        return optimalVelocity
+
+    print('caclulating optimal velocity')
+    # Calculate the radius for every point of the track
+    radius = []
+    for i in range(len(track)):
+        indexes = circle_indexes(track, i, add_index_1=-1, add_index_2=1)
+        coords = [track[indexes[0]],
+                  track[indexes[1]], track[indexes[2]]]
+        radius.append(circle_radius(coords))
+
+    # Get the max_velocity for the smallest radius
+    # That value should multiplied by a constant multiple
+    v_min_r = min(radius)**0.5
+    constant_multiple = min_speed / v_min_r
+
+    if look_ahead_points == 0:
+        # Get the maximal velocity from radius
+        max_velocity = [(constant_multiple * i**0.5) for i in radius]
+        # Get velocity from max_velocity (cap at MAX_SPEED)
+        velocity = [min(v, max_speed) for v in max_velocity]
+        return velocity
+
+    else:
+        # Looks at the next n radii of points and takes the minimum
+        # goal: reduce lookahead until car crashes bc no time to break
+        LOOK_AHEAD_POINTS = look_ahead_points
+        radius_lookahead = []
+        for i in range(len(radius)):
+            next_n_radius = []
+            for j in range(LOOK_AHEAD_POINTS+1):
+                index = circle_indexes(
+                    mylist=radius, index_car=i, add_index_1=j)[1]
+                next_n_radius.append(radius[index])
+            radius_lookahead.append(min(next_n_radius))
+        max_velocity_lookahead = [(constant_multiple * i**0.5)
+                                  for i in radius_lookahead]
+        velocity_lookahead = [min(v, max_speed)
+                              for v in max_velocity_lookahead]
+        
+        optimalVelocity = velocity_lookahead
+        return velocity_lookahead
 
 def _get_waypoints(params):
     global smoothPath
@@ -165,10 +244,9 @@ def is_higher_speed_favorable(params):
     max_speed = 3.8
     min_speed = 1.25
     sigma     = (max_speed-min_speed)/6
-    turn_angle = is_a_turn_coming_up(params)
-    optimal_speed = max_speed - ( turn_angle / max(angleChange)) * (max_speed - min_speed)
+    optimal_velocity_ = optimal_velocity( _get_waypoints(params), min_speed, max_speed, 10 )[ params['closest_waypoints'][1] ]
     # Calculate reward for speed
-    speed_diff = abs(params['speed'] - optimal_speed)
+    speed_diff = abs(params['speed'] - optimal_velocity_)
     reward_speed = math.exp(-0.5*((speed_diff**2)/sigma))
     return reward_speed*10
 
@@ -180,23 +258,93 @@ def is_steering_angle_correct(params):
         reward = reward/( 1+abs(steer) )
     return reward
 
-def is_steps_favorable(params):
-    '''
-    Example of using steps and progress
-    '''
+import math
 
-    # Read input variable
-    steps = params['steps']
-    progress = params['progress']
+MAX_TURN = 20
 
-    TOTAL_NUM_STEPS = 300
-    reward = 1.0
+def calc_angle_bw_heading(p1, p2, heading):
+    angle =  math.degrees(
+        math.atan2(p2[1] - p1[1], p2[0] - p1[0])
+    ) -  heading
+    if angle < 0:
+        angle = angle + 360
 
-    # Give additional reward if the car pass every 100 steps faster than expected
-    if steps > 0 and (steps % 100) == 0 and progress > (steps / TOTAL_NUM_STEPS) * 100 :
-        reward = 10.0
+    if angle > 180:
+        angle = angle - 360
+    
+    return angle
 
-    return float(reward)
+def calc_angle(p1, p2):
+    angle =  math.degrees(
+        math.atan2(p2[1] - p1[1], p2[0] - p1[0])
+    )
+    if angle < 0:
+        angle = angle + 360
+
+    if angle > 180:
+        angle = angle - 360
+    
+    return angle
+
+def calc_angle_bw_points(p1, p2, p3):
+    angle =  math.degrees(
+        math.atan2(p3[1] - p1[1], p3[0] - p1[0])
+    ) -  math.degrees(
+        math.atan2(p2[1] - p1[1], p2[0] - p1[0])
+    )
+    if angle < 0:
+        angle = angle + 360
+
+    if angle > 180:
+        angle = angle - 360
+    
+    return angle
+
+def get_future_heading(heading, steering_angle):
+    return heading + steering_angle
+
+# def steering_angle_factor(params):
+    MAX_TURN = 20
+
+    waypoints = _get_waypoints(params)
+    closest_waypoints = params['closest_waypoints']
+    heading = params['heading']
+    steering = params['steering_angle']
+    curr_point = (params['x'], params['y'])
+    next_point = waypoints[ (closest_waypoints[1]+1)%len(waypoints) ]
+    next_to_next_point = waypoints[ (closest_waypoints[1]+2)%len(waypoints) ]
+
+    # W.r.t heading
+    angle_of_coming_turn = calc_angle_bw_heading(curr_point, next_point, heading)
+    # turn between points
+    angle_bw_points = calc_angle_bw_points(curr_point, next_point, next_to_next_point)
+
+    # if abs(angle_of_coming_turn) > 90:
+    #     return 1e-6
+
+    if abs(angle_of_coming_turn) <= 4 and abs(angle_bw_points) <= 4:
+        # if angle of coming turn is less than 1 degrees meaning 
+        # more or less a straight line, penalize any steering angle
+        # above 0.5 degrees forcing the car to go straight
+        return 10 if steering < 4 else 1e-6
+
+    # if steering in opposite direction to angle, return negatively
+    
+    # If trck turns left and i turn right, i want to penalize
+    if ( angle_bw_points > 0 and steering < 0 ) or ( steering > 0 and angle_bw_points < 0 ):
+        return 1e-6    
+        
+    if abs(angle_bw_points) > MAX_TURN:
+        return 1e-6
+
+    # I feel the steering reward should be accoridng to angle_bw_points
+    # if it is close to angle_bw_points then reward it
+
+    # and heading reward should be close to angle_of_coming_turn
+    steering_reward = 10 * math.exp(-abs(steering - angle_bw_points) / 100)
+    heading_reward = 10 * math.exp(-abs(angle_of_coming_turn) / 100)
+    
+    return heading_reward * steering_reward
 
 def is_heading_correct(params):
     # Read input variables
@@ -235,7 +383,7 @@ def get_heading_reward(params):
     # Initialize the reward with typical value
     reward = 10
     if not is_heading_correct(params):
-        reward *= 0.001
+        reward *= 0.0001
     return float(reward)
 
 def following_smooth_path_reward(params):
@@ -252,12 +400,12 @@ def following_smooth_path_reward(params):
 
 def calc_sub_reward_and_aggregate(params):
     heading_reward          = get_heading_reward(params) # 0.01 to 10
-    steps_reward            = is_steps_favorable(params) # 1 or 10
     speed_reward            = is_higher_speed_favorable(params) # 1 to 10
     on_smooth_track_reward  = following_smooth_path_reward(params) # 0 to 10
-    reward                  = ( speed_reward + heading_reward + steps_reward + on_smooth_track_reward )**2 + ( speed_reward * heading_reward * steps_reward * on_smooth_track_reward )
-    reward                  = reward/100 # to ensure that the output is not crazily high
-    return reward
+    # reward                  = ( speed_reward + heading_reward + on_smooth_track_reward )**2 + ( speed_reward * heading_reward * on_smooth_track_reward )
+    # reward                  = reward/100 # to ensure that the output is not crazily high
+    reward = speed_reward + heading_reward + on_smooth_track_reward
+    return reward*10
 
 def calculate_reward(params):
     if params["is_offtrack"] or params["is_crashed"]:
